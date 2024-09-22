c95bac47d9712b08782b158a0193cc35e42a3ea5
John Cooke <jcooke@Johns-MBP.localdomain>
2024-09-22 18:23:04 +0100

    Restrict the words per row to 6

--- src/words.jsx
+++ src/words.jsx
@@ -4,15 +4,27 @@ import { Row } from "react-bootstrap";
 
 const Words = () => {
   const { words } = useWords();
+  const sortedWords = words.filteredWords.sort();
+
+  const rows = [...Array(Math.ceil(sortedWords.length / 6))];
+  // chunk the products into the array of rows
+  const wordRow = rows.map((row, idx) =>
+    sortedWords.slice(idx * 6, idx * 6 + 6)
+  );
+
+  // map the rows as row
+  const content = wordRow.map((row, idx) => (
+    <Row key={idx}>
+      {row.map((word) => (
+        <Word key={word} word={word} letters={words.letters}></Word>
+      ))}
+    </Row>
+  ));
 
   return (
     <section>
       <h3>Word count {words.filteredWords.length}</h3>
-      <Row>
-        {words.filteredWords.sort().map((word) => (
-          <Word key={word} word={word} letters={words.letters}></Word>
-        ))}
-      </Row>
+      {content}
     </section>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,21 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3022,
+  },
   test: {
+    api: {
+      port: 3023,
+    },
     environment: 'jsdom',
-    setupFiles: ['./tests/setupTests.js'],
-    globals: true
+    setupFiles: [
+      './test/setupTests.js',
+    ],
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    },
   }
 });
