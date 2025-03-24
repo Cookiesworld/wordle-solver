0e90fd69e6a75c672f88a90cd9d8722663e9644b
John Cooke <jcooke@Johns-MacBook-Pro.local>
2025-03-24 18:33:58 +0000

    Updated packages

--- src/appInsights.js
+++ src/appInsights.js
@@ -1,17 +1,12 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
-import { createBrowserHistory } from "history";
 const key = import.meta.env.VITE_APP_APPINSIGHTS_INSTRUMENTATIONKEY;
-const browserHistory = createBrowserHistory({ basename: '' });
 const reactPlugin = new ReactPlugin();
 const appInsights = new ApplicationInsights({
     config: {
         instrumentationKey: key,
         enableAutoRouteTracking: true,
-        extensions: [reactPlugin],
-        extensionConfig: {
-            [reactPlugin.identifier]: { history: browserHistory }
-        }
+        extensions: [reactPlugin]
     }
 });
 appInsights.loadAppInsights();
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
   server: {
     port: 3022,
   },
